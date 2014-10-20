c974ab649552cceb9a985b97a4dda6bac7423035
Alfred Morgan <amorganskate@yahoo.com>
2014-10-19 20:26:30 -0700

    Added Student Display Form
    -Added Student Display Form GUI

--- PeasantsProjectSolution/PeasantsProject/Student.Designer.vb
+++ PeasantsProjectSolution/PeasantsProject/Student.Designer.vb
@@ -23,7 +23,15 @@ Partial Class Main
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.dgvStudents = New System.Windows.Forms.DataGridView()
+        Me.dgvPackage = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSearchName = New System.Windows.Forms.TextBox()
+        Me.txtSearchEmail = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
         CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvPackage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvStudents
@@ -32,24 +40,99 @@ Partial Class Main
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.dgvStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
-        Me.dgvStudents.Location = New System.Drawing.Point(12, 42)
+        Me.dgvStudents.Location = New System.Drawing.Point(12, 171)
         Me.dgvStudents.Name = "dgvStudents"
         Me.dgvStudents.RowTemplate.Height = 24
-        Me.dgvStudents.Size = New System.Drawing.Size(598, 171)
+        Me.dgvStudents.Size = New System.Drawing.Size(495, 200)
         Me.dgvStudents.TabIndex = 0
+        '
+        'dgvPackage
+        '
+        Me.dgvPackage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPackage.Location = New System.Drawing.Point(513, 171)
+        Me.dgvPackage.Name = "dgvPackage"
+        Me.dgvPackage.RowTemplate.Height = 24
+        Me.dgvPackage.Size = New System.Drawing.Size(280, 200)
+        Me.dgvPackage.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "E-Mail:"
+        '
+        'txtSearchName
+        '
+        Me.txtSearchName.Location = New System.Drawing.Point(68, 23)
+        Me.txtSearchName.Name = "txtSearchName"
+        Me.txtSearchName.Size = New System.Drawing.Size(243, 22)
+        Me.txtSearchName.TabIndex = 4
+        '
+        'txtSearchEmail
+        '
+        Me.txtSearchEmail.Location = New System.Drawing.Point(70, 50)
+        Me.txtSearchEmail.Name = "txtSearchEmail"
+        Me.txtSearchEmail.Size = New System.Drawing.Size(241, 22)
+        Me.txtSearchEmail.TabIndex = 5
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(527, 137)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 28)
+        Me.btnAdd.TabIndex = 6
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(674, 137)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 7
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(622, 353)
+        Me.ClientSize = New System.Drawing.Size(805, 383)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtSearchEmail)
+        Me.Controls.Add(Me.txtSearchName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvPackage)
         Me.Controls.Add(Me.dgvStudents)
         Me.Name = "Main"
         Me.Text = "Student"
         CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvPackage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents dgvStudents As System.Windows.Forms.DataGridView
+    Friend WithEvents dgvPackage As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSearchName As System.Windows.Forms.TextBox
+    Friend WithEvents txtSearchEmail As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
 
 End Class
