63ebfe5924a65fc48ec18ce7ddc06e826ce2c461
Taylor Collins <collin016@morrisville.edu>
2014-11-04 00:40:22 -0500

    Updated Student Display Form

--- PeasantsProjectSolution/PeasantsProject/Student.Designer.vb
+++ PeasantsProjectSolution/PeasantsProject/Student.Designer.vb
@@ -24,115 +24,118 @@ Partial Class Main
     Private Sub InitializeComponent()
         Me.dgvStudents = New System.Windows.Forms.DataGridView()
         Me.dgvPackage = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtSearchName = New System.Windows.Forms.TextBox()
-        Me.txtSearchEmail = New System.Windows.Forms.TextBox()
-        Me.btnAdd = New System.Windows.Forms.Button()
         Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dgvPackage, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
         '
         'dgvStudents
         '
+        Me.dgvStudents.AllowUserToAddRows = False
+        Me.dgvStudents.AllowUserToDeleteRows = False
+        Me.dgvStudents.AllowUserToResizeColumns = False
+        Me.dgvStudents.AllowUserToResizeRows = False
         Me.dgvStudents.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.dgvStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
-        Me.dgvStudents.Location = New System.Drawing.Point(12, 171)
+        Me.dgvStudents.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised
+        Me.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvStudents.Location = New System.Drawing.Point(3, 3)
+        Me.dgvStudents.MultiSelect = False
         Me.dgvStudents.Name = "dgvStudents"
         Me.dgvStudents.RowTemplate.Height = 24
-        Me.dgvStudents.Size = New System.Drawing.Size(495, 200)
+        Me.dgvStudents.Size = New System.Drawing.Size(831, 318)
         Me.dgvStudents.TabIndex = 0
         '
         'dgvPackage
         '
-        Me.dgvPackage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvPackage.Location = New System.Drawing.Point(513, 171)
+        Me.dgvPackage.AllowUserToResizeRows = False
+        Me.dgvPackage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvPackage.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvPackage.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised
+        Me.dgvPackage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvPackage.Location = New System.Drawing.Point(3, 3)
+        Me.dgvPackage.MultiSelect = False
         Me.dgvPackage.Name = "dgvPackage"
         Me.dgvPackage.RowTemplate.Height = 24
-        Me.dgvPackage.Size = New System.Drawing.Size(280, 200)
+        Me.dgvPackage.Size = New System.Drawing.Size(836, 194)
         Me.dgvPackage.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 29)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(49, 17)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Name:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 50)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(51, 17)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "E-Mail:"
-        '
-        'txtSearchName
-        '
-        Me.txtSearchName.Location = New System.Drawing.Point(68, 23)
-        Me.txtSearchName.Name = "txtSearchName"
-        Me.txtSearchName.Size = New System.Drawing.Size(243, 22)
-        Me.txtSearchName.TabIndex = 4
-        '
-        'txtSearchEmail
-        '
-        Me.txtSearchEmail.Location = New System.Drawing.Point(70, 50)
-        Me.txtSearchEmail.Name = "txtSearchEmail"
-        Me.txtSearchEmail.Size = New System.Drawing.Size(241, 22)
-        Me.txtSearchEmail.TabIndex = 5
-        '
-        'btnAdd
-        '
-        Me.btnAdd.Location = New System.Drawing.Point(527, 137)
-        Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(75, 28)
-        Me.btnAdd.TabIndex = 6
-        Me.btnAdd.Text = "&Add"
-        Me.btnAdd.UseVisualStyleBackColor = True
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(674, 137)
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.Location = New System.Drawing.Point(433, 554)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
-        Me.btnDelete.TabIndex = 7
-        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.Size = New System.Drawing.Size(115, 24)
+        Me.btnDelete.TabIndex = 9
+        Me.btnDelete.Text = "&Delete Package"
         Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Location = New System.Drawing.Point(317, 554)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(110, 24)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "&Add Package"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SplitContainer1.IsSplitterFixed = True
+        Me.SplitContainer1.Location = New System.Drawing.Point(12, 12)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.dgvStudents)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.dgvPackage)
+        Me.SplitContainer1.Size = New System.Drawing.Size(841, 536)
+        Me.SplitContainer1.SplitterDistance = 328
+        Me.SplitContainer1.TabIndex = 10
         '
         'Main
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(805, 383)
+        Me.ClientSize = New System.Drawing.Size(865, 590)
+        Me.Controls.Add(Me.SplitContainer1)
         Me.Controls.Add(Me.btnDelete)
         Me.Controls.Add(Me.btnAdd)
-        Me.Controls.Add(Me.txtSearchEmail)
-        Me.Controls.Add(Me.txtSearchName)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.dgvPackage)
-        Me.Controls.Add(Me.dgvStudents)
         Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Student"
         CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.dgvPackage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents dgvStudents As System.Windows.Forms.DataGridView
-    Friend WithEvents dgvPackage As System.Windows.Forms.DataGridView
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents txtSearchName As System.Windows.Forms.TextBox
-    Friend WithEvents txtSearchEmail As System.Windows.Forms.TextBox
-    Friend WithEvents btnAdd As System.Windows.Forms.Button
     Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents dgvPackage As System.Windows.Forms.DataGridView
 
 End Class
--- PeasantsProjectSolution/PeasantsProject/Student.vb
+++ PeasantsProjectSolution/PeasantsProject/Student.vb
@@ -2,42 +2,115 @@
 
 Public Class Main
 
-
-    Dim dt As New DataTable
-    Dim filename As String = "C:\TEMP\Work.csv"
+    Dim dtStudents As New DataTable
+    Dim dtPacakges As New DataTable
+    Dim csvStudents As String = "\\CISNTS1\BUSINESS\CIT\Cita450\ho1046r-1.csv"
     Dim rowcount As Integer
-
+    Dim csvPackages As String = My.Application.Info.DirectoryPath & "\Packages.csv"
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(filename)
+        ' Load student table
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(csvStudents)
             MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
             MyReader.Delimiters = New String() {","}
             Dim firstrow As String()
             Dim currentRow As String()
-
             If Not MyReader.EndOfData Then
                 firstrow = MyReader.ReadFields()
                 For i As Integer = 0 To UBound(firstrow)
-                    dt.Columns.Add(firstrow(i))
+                    dtStudents.Columns.Add(firstrow(i))
                 Next
+                dtStudents.Columns.Add("# Package(s)")
             End If
+            dtStudents.Columns("# Package(s)").DefaultValue = 0
+
             While Not MyReader.EndOfData
                 Try
                     currentRow = MyReader.ReadFields()
-                    dt.Rows.Add(currentRow)
-
+                    dtStudents.Rows.Add(currentRow)
                 Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
                     MessageBox.Show(ex.Message & vbCrLf & "Skipping")
-
                 End Try
             End While
         End Using
-        dgvStudents.DataSource = dt
-        rowcount = dt.Rows.Count
+
+        ' Load package table
+        Using MyReader2 As New Microsoft.VisualBasic.FileIO.TextFieldParser(csvPackages)
+            MyReader2.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader2.Delimiters = New String() {","}
+            Dim firstrow As String()
+            Dim currentRow As String()
+            If Not MyReader2.EndOfData Then
+                firstrow = MyReader2.ReadFields()
+                For i As Integer = 0 To UBound(firstrow)
+                    dtPacakges.Columns.Add(firstrow(i))
+                Next
+            End If
+            dtPacakges.Columns(1).AutoIncrement = True
+            dtPacakges.Columns(1).Unique = True
+            dtPacakges.Columns(2).DataType = Type.GetType("System.Boolean")
+            dtPacakges.Columns(3).DataType = Type.GetType("System.DateTime")
+
+            While Not MyReader2.EndOfData
+                Try
+                    currentRow = MyReader2.ReadFields()
+                    dtPacakges.Rows.Add(currentRow)
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MessageBox.Show(ex.Message & vbCrLf & "Skipping")
+                End Try
+            End While
+        End Using
+
+        dgvStudents.DataSource = dtStudents
+        dgvPackage.DataSource = dtPacakges
+        rowcount = dtStudents.Rows.Count
+        dgvStudents.Rows(0).Selected = True
 
     End Sub
 
-   
-   
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs)
+        Try
+            SaveGridDataInFile(csvPackages, dgvPackage)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub dgvStudents_SelectionChanged(sender As Object, e As EventArgs) Handles dgvStudents.SelectionChanged
+        If dgvStudents.SelectedRows.Count < 1 Then Return
+
+        Dim studentIDvalue = dgvStudents.SelectedRows(0).Cells(0).Value
+
+        dgvPackage.Rows(dgvPackage.Rows.Count - 1).Cells(0).Value = studentIDvalue
+        dgvPackage.Rows(dgvPackage.Rows.Count - 1).Cells(1).Value = dgvPackage.RowCount
+    End Sub
+
+    Private Sub SaveGridDataInFile(ByVal fName As String, ByVal dgvA As DataGridView)
+        ''create a new stream(file) writer based on supplied path
+        'Dim fw As New IO.StreamWriter(fName, True)
+
+        ''row controller
+        'For x As Short = 0 To dgvA.Rows.Count - 1
+        '    'for each cell, write the cellvalue followed by a comma
+        '    For y As Short = 0 To dgvA.Columns.Count
+        '        fw.Write(dgvA.Rows.Item(x).Cells(y).Value + ",")
+        '    Next
+        '    'ends the line
+        '    fw.WriteLine()
+        'Next
+        ''close the file writer
+        'fw.Close()
+
+        Dim rows = From row As DataGridViewRow In dgvA.Rows.Cast(Of DataGridViewRow)() _
+                   Where Not row.IsNewRow _
+                   Select Array.ConvertAll(row.Cells.Cast(Of DataGridViewCell).ToArray, _
+                                           Function(c) If(c.Value IsNot Nothing, c.Value.ToString, ""))
+
+        Using sw As New IO.StreamWriter(fName, True)
+            For Each r In rows
+                sw.WriteLine(String.Join(",", r))
+            Next
+        End Using
+        Process.Start(fName)
+    End Sub
 End Class
